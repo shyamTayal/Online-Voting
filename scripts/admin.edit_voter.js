6289e71e59bc554f6ba5cbd0ee5ff388d5408160
Shyam Tayal <shyam.tayal@hotmail.com>
2021-04-24 18:18:30 +0530

    admin page half done 2/2

--- scripts/admin.edit_voter.js
+++ scripts/admin.edit_voter.js
@@ -48,5 +48,36 @@ function addVoter() {
 }
 function debarVoter(){
 
+    // For connecting Backend
+    /********************************/
+
+    var user = {
+        user_id: document.getElementById('dUserID').value,
+        my_id: myID??"TEMPUSER",
+        my_level: myLevel??2
+    }
+    var options = {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(user)
+    }
+    //
+    // fetch('http://localhost:5440/user/remove', options)
+    //     .then(res => {
+    //         res.json()
+    //             .then(data => {
+    //                 console.log(data)
+    //             })
+    //             .catch(err => console.log(`${err}`))
+    //     })
+    //     .catch(err => console.error(err))
+
+    /********************************/
+
+    document.forms[0].reset();
+
+    console.log('Debared', { user });
 
 }
