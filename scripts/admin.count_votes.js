55570889ffe90a0221fb883d676e11167e3dc4f7
pradhuman1 <pradhumansinghbaid@gmail.com>
2021-04-27 02:37:34 +0530

    count votes connected to backend

--- scripts/admin.count_votes.js
+++ scripts/admin.count_votes.js
@@ -1,6 +1,4 @@
 var input = document.querySelector('#elecID');
-myID = currUserObj["user_id"];
-myLevel = currUserObj["admin_level"];
 
 document.querySelector('#search_result').addEventListener('click',fetchResults)
 
@@ -13,18 +11,17 @@ async function fetchResults(){
             'Content-Type': 'application/json'
         },
         body: JSON.stringify({
-            my_id:myID,
-            my_level:myLevel,
-            election_ID: eID
+            election_id: eID
         })
     }
 
-    fetch('http://localhost:5440/candidate/listCandidate?action=2',options)
+    fetch('http://localhost:5440/election/results',options)
     .then((res)=>{
         res.json()
         .then((data)=>{
-            let results = data.actions;
-            console.log(results);
+            console.log(data)
+            let results = data.users;
+            // console.log(results);
             var i=1;
             results.forEach(curr => {
                 let html = `<td>${i}</td><td>${curr.candidate_id}</td><td>${curr.party}</td><td>${curr.name}</td><td>${curr.votes}</td>`
