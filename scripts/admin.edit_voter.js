a48221637eb8f061b2286ae7ee653097a30e0408
pradhuman1 <pradhumansinghbaid@gmail.com>
2021-04-27 13:18:14 +0530

    add/debar voter changed to add/debar user and connected to backend

--- scripts/admin.edit_voter.js
+++ scripts/admin.edit_voter.js
@@ -1,8 +1,5 @@
 let users = [];
 
-currUserObj = JSON.parse(window.localStorage.getItem("currUserObj"));
-myID = currUserObj["user_id"];
-myLevel = currUserObj["admin_level"];
 
 function addVoter() {
 
@@ -11,16 +8,10 @@ function addVoter() {
 
     var user = {
         user_id: document.getElementById('aUserID').value,
-        admin_level: 0,
-        my_id: myID??"TEMPUSER",
-        my_level: myLevel??2,
-        name: currUserObj["name"],
-        user_name: document.getElementById('fullName').value,
-        user_dob: document.getElementById('dob').value,
-        user_phno: document.getElementById('phNum').value,
-        user_gender: document.getElementById('gender').value,
-        user_address: document.getElementById('address').value,
+        password: document.getElementById('password').value,
+        admin_level: parseInt(document.querySelectorAll('input[name="role"]:checked')[0].value),
     }
+    console.log(user);
     var options = {
         method: 'POST',
         headers: {
@@ -29,22 +20,21 @@ function addVoter() {
         body: JSON.stringify(user)
     }
 
-    // fetch('http://localhost:5440/user/create', options)
-    //     .then(res => {
-    //         res.json()
-    //             .then(data => {
-    //                 console.log(data)
-    //             })
-    //             .catch(err => console.log(`${err}`))
-    //     })
-    //     .catch(err => console.error(err))
+    fetch('http://localhost:5440/user/create', options)
+        .then(res => {
+            res.json()
+                .then(data => {
+                    console.log(data)
+                })
+                .catch(err => console.log(`${err}`))
+        })
+        .catch(err => console.error(err))
 
     /********************************/
 
     users.push(user);
     document.forms[0].reset();
 
-    console.log('added', { users });
 }
 function debarVoter(){
 
@@ -53,8 +43,6 @@ function debarVoter(){
 
     var user = {
         user_id: document.getElementById('dUserID').value,
-        my_id: myID??"TEMPUSER",
-        my_level: myLevel??2
     }
     var options = {
         method: 'POST',
@@ -63,21 +51,21 @@ function debarVoter(){
         },
         body: JSON.stringify(user)
     }
-    //
-    // fetch('http://localhost:5440/user/remove', options)
-    //     .then(res => {
-    //         res.json()
-    //             .then(data => {
-    //                 console.log(data)
-    //             })
-    //             .catch(err => console.log(`${err}`))
-    //     })
-    //     .catch(err => console.error(err))
+    
+    fetch('http://localhost:5440/user/remove', options)
+        .then(res => {
+            res.json()
+                .then(data => {
+                    console.log(data)
+                })
+                .catch(err => console.log(`${err}`))
+        })
+        .catch(err => console.error(err))
 
     /********************************/
 
     document.forms[0].reset();
 
-    console.log('Debared', { user });
+    // console.log('Debared', { user });
 
 }
